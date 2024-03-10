9f06bf40f06f7249e80f150e684a4695c806fa8f
Rupal Malhotra <rupalmalhotra4803@gmail.com>
2024-03-10 15:35:28 +0530

    9th march 2024

--- LeetcodeDaily/20240309_2540_MinimumCommonValue.cpp
+++ LeetcodeDaily/20240309_2540_MinimumCommonValue.cpp
@@ -3,11 +3,17 @@
 
 using namespace std;
 
+
+// Two bullshit approaches are
+// 1. Binary search elemnts of one array into another
+// 2. Use a hashmap or hashset and then search for the elements in that hashmap
+//     (Although the TC is same as Optimal it has higher SC)
+
+
+// 2-pointer Approach
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        
-        // Doint it as Daily Question
 
         int index1 = 0; // Pointer for 1st array
         int index2 = 0; // POinter for 2nd array
