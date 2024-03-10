6a1a5829a03c55bdf9616654abd2e6e2518c6e2f
Rupal Malhotra <rupalmalhotra4803@gmail.com>
2024-03-10 13:31:53 +0530

    10th march 2024

---
+++ GfgDaily/20240310_RemoveAllDuplicatesFromAGivenString.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    // code here
+	    string ans;
+	    unordered_set<char> s;
+	    for (auto x : str) {
+	        if (s.find(x) == s.end()) {
+	            s.insert(x);
+	            ans.push_back(x);
+	        }
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
